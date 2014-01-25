9a130e711768533ff1bbab2878421f92fc808f38
Astr0surf3r <astr0surf3r@gmail.com>
2014-01-25 19:08:28 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloog::Application.config.secret_token = 'cc1e14321bb5ac3d70b87d60de4f72b081a898c23415e2ea9564acc81a97b93e63a2e04c9514dc8374af5059c64e183fe58a9b8442f3596f633708898a932fcb'
